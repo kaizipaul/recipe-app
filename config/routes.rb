4a62fe675f035b8db92167570259671233eb4299
blase147 <solarmails2@gmail.com>
2023-02-28 19:13:04 +0100

    Update routes.rb, Update schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-    devise_for :users
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-resources :recipes, except: [:update] do
+  resources :recipes, except: [:update] do
   end
 
   resources :foods, except: [:update] do
@@ -11,5 +9,3 @@ resources :recipes, except: [:update] do
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
-
