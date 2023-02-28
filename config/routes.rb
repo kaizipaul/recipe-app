a531364525bb4d93ed11ae5bf52bcd964be49a22
blase147 <solarmails2@gmail.com>
2023-02-28 18:30:40 +0100

    Add Food list

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+resources :recipes, except: [:update] do
+  end
+
+  resources :foods, except: [:update] do
+  end
+
+  # Defines the root path route ("/")
   # root "articles#index"
 end
+
