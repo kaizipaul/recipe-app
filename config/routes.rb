d2599d8910290e6e12ec09431615c66ca8c60faa
blase147 <solarmails2@gmail.com>
2023-02-28 17:11:17 +0100

    Modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+resources :recipes, except: [:update] do
+  end
+
+  resources :foods, except: [:update] do
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
