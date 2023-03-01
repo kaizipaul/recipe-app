89f24cb12882a01ee4e0dba6c2669805715eb85c
blase147 <solarmails2@gmail.com>
2023-03-01 15:24:17 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :recipes, except: [:update] do
+
+  resources :recipes do
+    resources :recipe_foods, as: 'foods'
+    # resources :shopinglists
   end
   resources :public_recipes
   post 'toggle_public', to: 'recipes#toggle'
@@ -11,9 +14,6 @@ Rails.application.routes.draw do
 
   resources :shoppinglist, only: %i[index]
 
-  resources :foods, except: [:update] do
-  end
-end
   # Defines the root path route ("/")
   # root "articles#index"
 
