fcbf5dceecd4d86856c7ed6fd0c31a69a4d36c45
blase147 <solarmails2@gmail.com>
2023-03-01 01:52:25 +0100

    Fix authorizatio n + 1 problem

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,24 +1,31 @@
 class FoodsController < ApplicationController
+  load_and_authorize_resource
+
   before_action :set_food, only: %i[show edit update destroy]
+  before_action :find_user
 
   def index
-    @foods = Food.all
+    @foods = @user.foods.all
   end
 
   def show; end
-
   def new
     @food = Food.new
+    puts "Recipe #{params[:recipe_id]}"
   end
-
   def edit; end
-
   def create
     @food = Food.new(food_params)
-
+    @food.user = @user
     respond_to do |format|
       if @food.save
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
+        if params[:recipe_id].nil?
+          format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
+        else
+          format.html do
+            redirect_to recipe_foods_path(params[:recipe_id]), notice: 'Food for recipe added successfully.'
+          end
+        end
         format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -26,7 +33,6 @@ class FoodsController < ApplicationController
       end
     end
   end
-
   def update
     respond_to do |format|
       if @food.update(food_params)
@@ -38,22 +44,27 @@ class FoodsController < ApplicationController
       end
     end
   end
-
   def destroy
-    @food.destroy
-
+    food_recipes_count = RecipeFood.where(food_id: @food.id).count
     respond_to do |format|
-      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
+      if food_recipes_count.positive?
+        format.html do
+          redirect_to foods_url, notice: "You cannot remove this food. #{food_recipes_count} recipies have it."
+        end
+      else
+        @food.destroy
+        format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
+      end
       format.json { head :no_content }
     end
   end
-
   private
-
+  def find_user
+    @user = current_user
+  end
   def set_food
     @food = Food.find(params[:id])
   end
-
   def food_params
     params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :recipes, except: [:update] do
+  resources :recipes do
+    resources :recipe_foods, as: 'foods'
+    # resources :shopinglists
   end
-
-  resources :foods, except: [:update] do
-  end
-end
+  resources :public_recipes
+  post 'toggle_public', to: 'recipes#toggle'
+  resources :foods
+  resources :shoppinglist, only: %i[index]
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/unauthorized', to: 'unauthorized#index'
+
+  root "foods#index"
+  end
